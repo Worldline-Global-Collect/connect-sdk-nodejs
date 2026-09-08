20943b01d518c248c96d25990c391e324f3dc20e
WorldlineConnect <github.connect@worldline.com>
2026-09-08 10:37:37 +0200

    Release 8.3.2

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -18,7 +18,7 @@ interface ServerMetaInfo {
 export function serverMetaInfo(sdkContext: SdkContext): Header {
   const info: ServerMetaInfo = {
     sdkCreator: "Worldline",
-    sdkIdentifier: "NodejsServerSDK/v8.3.1",
+    sdkIdentifier: "NodejsServerSDK/v8.3.2",
     platformIdentifier: `${process.env["OS"]} Node.js/${process.versions.node}`,
     integrator: sdkContext.getIntegrator()
   };
